55625c2b124a36dc0ae271a3edeff63220619526
suprunov <rocknroll2418@gmail.com>
2021-07-11 18:11:03 +0300

    React p3

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -3,9 +3,15 @@ import classes from './Auth.module.css'
 import Button from "../../components/UI/Button/Button";
 import Input from "../../components/UI/Input/Input";
 
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
 export default class Auth extends Component {
 
     state = {
+        isFormValid: false,
         formControls: {
             email: {
                 value: '',
@@ -45,8 +51,44 @@ export default class Auth extends Component {
     submitHandler = event => {
         event.preventDefault()
     }
+
+    validateControl (value,validation) {
+        if (!validation) {
+            return true
+        }
+        let isValid = true
+        if (validation.required) {
+            isValid = value.trim() !== '' && isValid
+        }
+        if (validation.email) {
+            isValid = validateEmail(value) && isValid
+        }
+        if (validation.minLength) {
+            isValid = value.length >= validation.minLength && isValid
+        }
+
+        return isValid
+    }
+
     onChangeHandler = (event, controlName) => {
-        console.log(`${controlName}:`, event.target.value)
+
+        const formControls = { ...this.state.formControls }
+        const control = { ...formControls[controlName] }
+
+        control.value= event.target.value
+        control.touched = true
+        control.valid = this.validateControl(control.value, control.validation)
+
+        formControls[controlName] = control
+
+        let isFormValid = true
+        Object.keys(formControls).forEach(name => {
+            isFormValid = formControls[name].valid && isFormValid
+        })
+
+        this.setState({
+            formControls, isFormValid
+        })
     }
 
     renderInputs() {
@@ -81,6 +123,7 @@ export default class Auth extends Component {
                        <Button
                            type="success"
                            onClick={this.loginHandler}
+                           disabled={!this.state.isFormValid}
                        >
                            Войти
                        </Button>
